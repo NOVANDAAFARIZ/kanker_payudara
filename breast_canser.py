4b1bdd7c314cbd1fee0ad34210243b4f0aa5a114
NOVANDAAFARIZ <119787348+NOVANDAAFARIZ@users.noreply.github.com>
2025-04-27 21:57:43 +0700

    Update breast_canser.py

--- breast_canser.py
+++ breast_canser.py
@@ -100,8 +100,8 @@ st.markdown("""
 
     /* Gaya untuk setiap opsi dalam dropdown */
     .stSelectbox div[data-baseweb="option"] {
-        color: #ffffff !important; /* Warna teks putih untuk opsi */
-        background-color: #333333 !important; /* Latar belakang opsi */
+        color: #333333 !important; /* Warna teks putih untuk opsi */
+        background-color: #ffffff !important; /* Latar belakang opsi */
         padding: 10px !important;
         font-size: 18px !important;
     }
